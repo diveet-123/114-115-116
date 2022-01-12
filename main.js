1792a97f06a658b8bae73d636a46cfd9419b5f66
diveet-123 <84632888+diveet-123@users.noreply.github.com>
2022-01-12 10:25:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,8 +7,10 @@ function setup()
     canvas=createCanvas(350,350);
     canvas.center();
     video=createCapture(VIDEO);
-    video.size(300 , 300)
+    video.size(300 , 300);
     video.hide();
+    poseNet = ml5.poseNet(video , modelLoaded);
+    poseNet.on("pose",gotResults);
 }
 
 function draw()
@@ -16,7 +18,22 @@ function draw()
     image(video , 0 , 0 , 300 , 300)
 }
 
+function modelLoaded()
+{
+    console.log("poseNet is Initialized");
+}
+
+function gotResults(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("nose x = ", results[0].pose.nose.x);
+        console.log("nose y = ", results[0].pose.nose.y);
+    }
+}
+
 function take_snapshot()
 {
-    save('Snapshot.png');
+    save('taked_snapshot.png')
 }
